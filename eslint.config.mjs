c08a7d69153b801526b53e6dae32ee93ee9144fb
eo9on9 <eo9on9@gmail.com>
2025-12-29 09:37:30 +0900

    chore: set lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,46 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import js from '@eslint/js'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
+import pluginReact from 'eslint-plugin-react'
+import pluginReactHooks from 'eslint-plugin-react-hooks'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import tseslint from 'typescript-eslint'
 
 const eslintConfig = defineConfig([
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: {
+      'react-hooks': pluginReactHooks,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  eslintConfigPrettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
-]);
+])
 
-export default eslintConfig;
+export default eslintConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import styles from '@/styles/Home.module.css'
+import { Geist, Geist_Mono } from 'next/font/google'
+import Head from 'next/head'
+import Image from 'next/image'
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
 
 export default function Home() {
   return (
@@ -37,22 +37,22 @@ export default function Home() {
           <div className={styles.intro}>
             <h1>To get started, edit the index.tsx file.</h1>
             <p>
-              Looking for a starting point or more instructions? Head over to{" "}
+              Looking for a starting point or more instructions? Head over to{' '}
               <a
                 href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 Templates
-              </a>{" "}
-              or the{" "}
+              </a>{' '}
+              or the{' '}
               <a
                 href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 Learning
-              </a>{" "}
+              </a>{' '}
               center.
             </p>
           </div>
@@ -84,5 +84,5 @@ export default function Home() {
         </main>
       </div>
     </>
-  );
+  )
 }
