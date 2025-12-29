5dbb612d546ce47664282993ffa6e36e49d2a562
eo9on9 <eo9on9@gmail.com>
2025-12-29 13:20:01 +0900

    fail case

--- sources/shared/utils/add.test.ts
+++ sources/shared/utils/add.test.ts
@@ -5,4 +5,7 @@ describe('add', () => {
     expect(add(1, 2)).toBe(3)
     expect(add(1, 2)).not.toBe(4)
   })
+  it('should add two numbers: failed', () => {
+    expect(add(1, 2)).toBe(4)
+  })
 })
